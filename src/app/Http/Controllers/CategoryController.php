1cd8cd82e6f6dfa25a0bd4d1a53faab2040037a7
SebaJeldres <Sjeldresosoriodev@gmail.com>
2026-07-22 13:05:45 -0400

    feat: implementar métodos para crear y almacenar categorías en CategoryController

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
-    //
+    public function index() {
+        $categories = Category::with('manager')->paginate(10);
+        return view('categories.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        $managers = User::role(['manager, admin'])->get();
+        return view('categories,create', compact('managers'));
+
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255|unique:categories,name',
+            'description' => 'nullable|string',
+            'user_id' => 'nullable|exists:users,id',
+        ]);
+
+        Category::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+            'description' => $request->description,
+            'user_id' => $request->user_id,
+        ]);
+
+        return redirect()->route('categories.index')->with('success', 'Categoria creada exitosamente');
+
+    }
 }
